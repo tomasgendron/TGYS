6ba63c908e70f9d388fe0f95988703da42936e5a
Tomas Gendron <tomas.gendron@gmail.com>
2018-04-06 14:54:41 -0600

    rotator code

--- Clients/CVB/Scripts/PSRotator.js
+++ Clients/CVB/Scripts/PSRotator.js
@@ -1,53 +1,28 @@
 /*
+
 this code runs against this html from your page
 
-<table id="mainWebPartTable">
-	<tr>
-		<td>
-			<div id="PSRotator" class='PSRotator'>
-				<table>
-					<tr>
-						<td valign='top' id="rotLeft">
-							<div id="rotLeftImage" style='float:left;'>
-							</div>
-						</td>
-						<td valign='top' id="rotCentre">
-							<div style='float:right;'>
-								<h2 id='rotLeftTitle'></h2>
-								<h3 id='rotLeftSubTitle'></h3>
-								<p id='rotLeftContent'></p>
-							</div>
-						</td>
-						<td valign='top' id="rotRight">
-							<table id="rotRightTable" cellspacing="0" cellpadding="0">
-							</table>
-						</td>
-					</tr>
-				</table>
-			</div>
-		</td>
-		<td>
-		</td>
-		<td>
-			<div id="PSRightNav">
-				<div class='homePageCategory'>CATEGORY 1</div>
-				<div linkcat="Category 1"></div>
-				<div class='homePageCategory'>CATEGORY 2</div>
-				<div linkcat="Category 2"></div>
-				<div class='homePageCategory'>CATEGORY 3</div>
-				<div linkcat="Category 3"></div>
-			</div>
-		</td>
-	</tr>
-</table>
+<div id="PSRotator" class='PSRotator'>
+<table><tr><td valign='top' id="rotLeft"><div id="rotLeftImage" style='float:left;'> </div></td>
+<td valign='top' id="rotCentre">
 
-<script>
-function runWhenJQueryLoaded()
-{
-    $.getScript( _spPageContextInfo.siteAbsoluteUrl + "/SiteAssets/PSV/js/PSRotator.js" );
-     
-}
-</script>
+<div style='float:right;'>
+<h2 id='rotLeftTitle'></h2>
+<h3 id='rotLeftSubTitle'></h3>
+<p id='rotLeftContent'></p>
+</div>
+  
+</td>
+
+<td valign='top' id="rotRight">
+<table id="rotRightTable" cellspacing="0" cellpadding="0">
+
+</table>
+</td>
+
+</tr></table>
+</div>
+
 
 */
 
@@ -58,13 +33,12 @@ var PSRotator = window.PSRotator ||
   {
 	  _rdata=null;
 	  _rotIdx = 1;
-	  _maxItems = 2; // this will control how many items are shown in the rotator. make sure the list has at least this many items!
 	  
 	  // this func will stop the timer from fliping news items
 	  function SetRotatorMemStop(idx)
 	  {
-		  //clearInterval(myTimerId);
-		  //SetRotatorMem(idx)
+		  clearInterval(myTimerId);
+		  SetRotatorMem(idx)
 		  
 	  }
 	  
@@ -72,35 +46,21 @@ var PSRotator = window.PSRotator ||
 	  {
 				rdata=_rdata;
 
-				// we have 2 and only 2 items
-				$("a.hpnLink1").attr('href', rdata.d.results[0].MoreLink);
-				$("a.hpnLink2").attr('href', rdata.d.results[1].MoreLink);
-
-				$("img.hpnImg1").attr('src', rdata.d.results[0].NewsImage.Url);
-				$("img.hpnImg2").attr('src', rdata.d.results[1].NewsImage.Url);
-
-				$("span.hpnTitle1").html(rdata.d.results[0].Title);
-				$("span.hpnTitle2").html(rdata.d.results[1].Title);
-
-				$("p.hpnText1").html(rdata.d.results[0].NewsText.replace(/\n/g,"<br>"));
-				$("p.hpnText2").html(rdata.d.results[1].NewsText.replace(/\n/g,"<br>"));
-
-
-// 				$("#rotLeftImage").html("<img class='rotImage' src='"+ rdata.d.results[idx].LeftImage.Url +"' />" );
-// 				$("#rotLeftContent").html(rdata.d.results[idx].Content + "<br><br><a href='" + rdata.d.results[idx].ReadMoreURL.Url + "'>" + rdata.d.results[idx].ReadMoreURL.Description +"</a>");
-// 				$("#rotLeftTitle").html(rdata.d.results[idx].Title);
-// 				$("#rotLeftSubTitle").html(rdata.d.results[idx].Sub_x0020_Title);
+				$("#rotLeftImage").html("<img class='rotImage' src='"+ rdata.d.results[idx].LeftImage.Url +"' />" );
+				$("#rotLeftContent").html(rdata.d.results[idx].Content + "<br><br><a href='" + rdata.d.results[idx].ReadMoreURL.Url + "'>" + rdata.d.results[idx].ReadMoreURL.Description +"</a>");
+				$("#rotLeftTitle").html(rdata.d.results[idx].Title);
+				$("#rotLeftSubTitle").html(rdata.d.results[idx].Sub_x0020_Title);
 				
-// 				// set right sidebar
-// 				$("#rotRightTable").empty();
-// ;	
+				// set right sidebar
+				$("#rotRightTable").empty();
+;	
 
-// 				$(rdata.d.results).each(function(i){
-// 					if(i==idx)
-// 						$("#rotRightTable").append("<tr><td><img src='"+  _spPageContextInfo.siteAbsoluteUrl  +"/SiteAssets/PSV/images/arrow.gif' /></td><td class='PSRotSelected' >" + this.Title + "<br>" + this.Sub_x0020_Title  +"</td></tr>");
-// 					else
-// 						$("#rotRightTable").append("<tr><td> </td><td class='PSRotNotSelected'><a href='javascript:PSRotator.Lib.SetRotatorMemStop("+ i +")'>" + this.Title + "</a><br>" + this.Sub_x0020_Title  +"</td></tr>");
-// 				});			  
+				$(rdata.d.results).each(function(i){
+					if(i==idx)
+						$("#rotRightTable").append("<tr><td><img src='"+  _spPageContextInfo.siteAbsoluteUrl  +"/SiteAssets/images/arrow.gif' /></td><td class='PSRotSelected' >" + this.Title + "<br>" + this.Sub_x0020_Title  +"</td></tr>");
+					else
+						$("#rotRightTable").append("<tr><td> </td><td class='PSRotNotSelected'><a href='javascript:PSRotator.Lib.SetRotatorMemStop("+ i +")'>" + this.Title + "</a><br>" + this.Sub_x0020_Title  +"</td></tr>");
+				});			  
 		  
 	  }
 	  
@@ -124,11 +84,9 @@ var PSRotator = window.PSRotator ||
 	   }
 	  
 		SetRotator(0);
-
-		// limit is set by the $top only
 		function LoadRotatorData()
 		{
-			var p = RunAjax(_spPageContextInfo.siteAbsoluteUrl + "/_api/web/lists/GetByTitle('HomePageNews')/items?$orderby=Order0&$top=" + _maxItems);
+			var p = RunAjax(_spPageContextInfo.siteAbsoluteUrl + "/_api/web/lists/GetByTitle('HomePageNews')/items?$orderby=Order0&$top=5");
 			p.done(function(rdata){
 				console.log(rdata);
 				_rdata = rdata;
@@ -137,16 +95,16 @@ var PSRotator = window.PSRotator ||
 			
 		}
 
-		// var myTimerId = setInterval(myTimer, 60000);
+		var myTimerId = setInterval(myTimer, 20000);
 
-		// function myTimer() {
+		function myTimer() {
 
-		// 	SetRotator(_rotIdx);
-		// 	_rotIdx+=1;
-		// 	if(_rotIdx>= _maxItems)
-		// 		_rotIdx=0;
-		// 	//
-		// }	
+			SetRotator(_rotIdx);
+			_rotIdx+=1;
+			if(_rotIdx>=5)
+				_rotIdx=0;
+			//
+		}	
 
 		// ================================================
 		//  DataAccess library
