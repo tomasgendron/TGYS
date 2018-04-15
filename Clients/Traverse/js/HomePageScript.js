926bd7b1ef94d06b682c6c4d33901df6e4115c83
Yogendra Saxena <saxyogi@yahoo.com>
2018-04-15 00:05:12 -0600

    Added code for Traverse

---
+++ Clients/Traverse/js/HomePageScript.js
@@ -0,0 +1,93 @@
+<script>
+var PS = window.PS || {};
+PS.Lib = function () {
+	var attemptCount = 0;
+	var subhash = {}; // by vendor
+	var _numberOfUpcomingEventsToDisplay = 7;
+	var RunAjax = function (restUrl) {
+		//var nocache = new Date().getTime();
+		return $.ajax({
+			url: restUrl,
+			dataType: 'json',
+			type: 'GET',
+			headers: {"accept": "application/json;odata=verbose"}
+		})
+	}
+
+	var waitForJQuery = function () {
+		attemptCount++;
+		if (typeof jQuery != 'undefined') { // JQuery is loaded!
+			PS.Lib.initCode();
+			return;
+		}
+		if (attemptCount < 100) {
+			setTimeout(waitForJQuery, 100); // Check 10x a second
+		}
+		return;
+	}
+
+	var loadLeftNav = function () {
+		//$("div#PSLeftNav").html("loaded");
+		$("div#PSLeftNav").empty();
+		var p = RunAjax(_spPageContextInfo.siteServerRelativeUrl + "/_api/web/lists/getByTitle('HomepageMenu')/items?$filter=Location eq 'Left'");
+		p.done(function (data) {
+			console.log(data);
+			$.each(data.d.results, function () {
+				$("div#PSLeftNav").append("<div style='color:#" + this.TextColor + ";background-color:#" + this.BgdColor + "'><a href='" + this.Url + "'>" + this.Title + "</a></div>")
+			});
+		});
+	}
+
+	var loadRightNav = function () {
+		//_api/web/lists/getByTitle('UpcomingEvents')/items?$orderby=Event%5Fx0020%5FDate
+		//$("div#PSRightNav").empty();
+		var p = RunAjax(_spPageContextInfo.siteAbsoluteUrl + "/_api/web/lists/getByTitle('HomepageMenu')/items?$filter=Location eq 'Right'&$orderby=LinkOrder");
+		p.done(function (data) {
+			console.log(data);
+			$.each(data.d.results, function () {
+				//$("div#PSRightNav").append("<div style='color:#"+ this.TextColor +";background-color:#"+ this.BgdColor +"'><a href='"+ this.Url +"'>"+ this.Title+"</a></div>")
+				$("div[linkcat='" + this.LinkCategory + "']").append("<div style='color:#" + this.TextColor + ";background-color:#" + this.BgdColor + "'><a href='" + this.Url + "'>" + this.Title + "</a></div>");
+			});
+		});
+		var today = new Date();
+		//ge datetime'" + today.toISOString()
+		var pEvents = RunAjax(_spPageContextInfo.siteAbsoluteUrl + "/teams/Company/_api/web/lists/getByTitle('Calendar')/items?$top=" + _numberOfUpcomingEventsToDisplay + "&$filter=EventDate ge datetime'" + today.toISOString() + "'&$orderby=EventDate");
+		//var pEvents= RunAjax(_spPageContextInfo.siteAbsoluteUrl  + "/_api/web/lists/getByTitle('UpcomingEvents')/items?$filter=Event%5Fx0020%5FDate ge datetime'" + today.toISOString() +"'&$orderby=Event%5Fx0020%5FDate");
+		pEvents.done(function (data) {
+			//console.log(data);
+			$.each(data.d.results, function () {
+				var myDate = new Date(this.EventDate);
+				myDate.setHours(myDate.getHours() + 7);
+				var myDate2 = myDate.format("MMM dd yyyy");
+				$("div[linkcat='Upcoming Events']").append("<div class='upcomingEventItem'><span class='upcomingeventtext'>" + this.Title + "</span><span class='upcomingeventDate'>" + myDate2 + "</span></div>");
+			});
+		});
+	}
+
+	var initCode = function () {
+		$(document).ready(function () {
+			//$("#pageTitle").before("Our World: The CBV Intranet Portal");
+			loadLeftNav();
+			loadRightNav();
+			// check if in edit mode
+			var inDesignMode = document.forms[MSOWebPartPageFormName].MSOLayout_InDesignMode.value;
+			if (inDesignMode == "1") {
+				$("#s4-ribbonrow").attr("height", "35px");
+				$("#s4-ribboncont").show();
+			}
+			if (typeof runWhenJQueryLoaded == 'function') {
+				runWhenJQueryLoaded();
+			}
+		})
+	}
+
+	// public interface
+	return {
+		subhash: subhash,
+		initCode: initCode,
+		waitForJQuery: waitForJQuery
+	};
+}();
+
+PS.Lib.waitForJQuery();
+</script>
---
+++ Clients/Traverse/ps/MegaMenuInstallationSPOption.ps1
@@ -0,0 +1,94 @@
+﻿ if ((Get-PSSnapin | Where-Object {$_.Name -eq 'Microsoft.SharePoint.Powershell'})-eq $null) 
+    {
+    	Add-PSSnapin "Microsoft.SharePoint.Powershell"
+    }
+
+<# This script copies all contents from SiteAssets Library of Root Site collection to Target Site collection.
+It is expected from admin to open RegisterScript.aspx after running this script from 
+Target Site Collection's SiteAssets library\Scripts folder and click on "Auto Install All Needed Actions" button.
+#>
+
+# Varaibles definitions and user inputs
+Write-Host "If Target Site Collection is not a clean/new site collection, then please delete branding files from 'Site Assets' library of Target Site Collection. This Utility will not override any file or folder and will display an error if file already exists."
+
+[void](Read-Host 'Press Enter to continue…')
+
+$targetSiteCollection= Read-Host "Please enter Target Site Collection URL, where you want mega menu and branding to be applied (Example: https://travind.sharepoint.com/teams/Site1)"
+if($targetSiteCollection){
+    $sourceSiteCollection = "https://travind.sharepoint.com"
+    $sourceAssetLibraryTitle = "Site Assets"
+    #$targetSiteCollection = "https://travind.sharepoint.com/teams/Site1"
+    $targetAssetLibraryTitle = "Site Assets"
+
+    # Get Site Collections here
+    $sourceWeb = Get-SPWeb $sourceSiteCollection
+
+    #To dectivate MDS feature 
+    If($destinationWeb.EnableMinimalDownload -eq 1)
+    {
+        Write-Host "Minimal Download Strategy is Enabled for this site!";
+        Write-Host "Disabling....";
+        Disable-SPFeature –identity "MDSFeature" -URL $destinationWeb.URL -confirm:$false
+    }   
+
+    # Get list objects here
+    $sourceList = $sourceWeb.Lists | ? {$_.Title -eq $sourceAssetLibraryTitle}
+    # Get Site Collections here
+    $destinationWeb = Get-SPWeb $targetSiteCollection
+    # Get list objects here
+    $destinationList = $destinationWeb.Lists | ? {$_.title -like $targetAssetLibraryTitle}
+    $allFolders = $sourceList.Folders
+    $rootFolder = $sourceList.rootFolder
+    $rootItems = $rootFolder.files
+    #Loop
+    foreach($rootItem in $rootItems)
+    {
+        $sourceStream = $rootItem.OpenBinary()
+        $destinationFileItem = $destinationList.rootFolder.Files.Add($rootItem.Name, $sourceStream, $true)
+        $destinationFileItem.Update()
+    }
+
+    #Loop for rest of the folders
+
+    foreach($folder in $allFolders)
+    {
+        Remove-Variable ParentFolderURL
+        $cnt = 0
+        $folderURL = $folder.url.Split("/")
+        while($cnt -lt ($folderURL.count-1))
+        {
+        $ParentFolderURL = "$ParentFolderURL/" + $folderURL[$cnt]
+        $cnt++
+        }
+        
+        $currFolder = $destinationList.Folders | ? {$_.url -eq $ParentFolderURL.substring(1)}
+        if(!($currFolder.Folders | ? {$_.name -eq $folder.Name}))
+        {
+            $tmpFolder = $destinationList.Folders.Add(("$targetSiteCollection" + $ParentFolderURL), [Microsoft.SharePoint.SPFileSystemObjectType]::Folder, $folder.name)
+            $tmpFolder.update()
+        }
+        else
+        {
+            $tmpFolder = $destinationList.Folders | ? {$_.name -eq $folder.Name}
+        }
+        
+        $allFiles = $sourceList.Items
+        $sourceItems = $folder.folder.Files
+        
+        if($folder.Folder.Files.count -gt 0)
+        {
+            foreach($item in $sourceItems)
+            {
+                $Relative = ($Item.ServerRelativeUrl).substring(1)
+                $TargetItem = $allFiles | ? {$_.URL -eq $Relative}
+                $sourceStream = $TargetItem.File.OpenBinary()
+                $destinationFileItem = $tmpFolder.Folder.Files.Add($TargetItem.Name, $sourceStream, $true)
+                $destinationFileItem.Update()
+            }
+        }
+    }
+    Write-Host "Deployement is successful!"
+}
+else{
+    Write-Host "Please make sure that Target Site Collection Url is valid, exiting...!"
+}
